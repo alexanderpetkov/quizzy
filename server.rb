206176aab8478e5ae83f3eaa6cc58b41816c0544
Alexander Milchev <amilchev@asteasolutions.com>
2015-02-02 17:47:37 +0200

    User erb for templating

--- server.rb
+++ server.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/activerecord'
 
+Dir['lib/*.rb'].each { |file| require_relative file }
 Dir['models/*.rb'].each { |file| require_relative file }
+Dir['controllers/*.rb'].each { |file| require_relative file }
 
 set :port, 1889
 set :bind, '0.0.0.0'
-# set :public_folder, proc { File.join(root, 'public') }
 
 get '/' do
-  redirect 'index.html', 303
+  erb :index
 end
-
