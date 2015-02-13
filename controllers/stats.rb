0410a5f4cc59e9c6e35739e2a0cd2d32962bfcb7
Alexander Milchev <amilchev@asteasolutions.com>
2015-02-14 01:31:19 +0200

    Make pie chart dynamic in my stats

--- controllers/stats.rb
+++ controllers/stats.rb
@@ -10,10 +10,24 @@ namespace '/stats' do
       "#{result}.\n#{Question.find(answering.question_id).text_representation}"
     end
 
+    if category
+      all_entries = QuestionsUsers.filter_by_category(category).select do |entry| 
+        entry.user_id == user.id
+      end
+
+      correct_count = all_entries.select { |entry| entry.is_correct == true }.size
+
+      chart_data = {
+        'correct' => correct_count,
+        'mistaken' => all_entries.size - correct_count
+      }
+    end
+
     erb :stats, locals: {
       is_checked: category,
       stats: textified_stats,
       categories: Category.all.map(&:name),
+      chart_data: chart_data,
       user_id: user.id
     }
   end
--- models/questions_users.rb
+++ models/questions_users.rb
@@ -1,2 +1,8 @@
 class QuestionsUsers < ActiveRecord::Base
+  def self.filter_by_category(category_name)
+    QuestionsUsers.select do |entry|
+      question = Question.find(entry.question_id)
+      question.categories.map(&:name).include? category_name
+    end
+  end
 end
